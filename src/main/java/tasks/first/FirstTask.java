52835c3dc78ea42ca5fdf5aca761c302ddc14281
zuukhraa <79767470+zuukhraa@users.noreply.github.com>
2021-03-11 20:35:28 +0300

    Initial commit

---
+++ src/main/java/tasks/first/FirstTask.java
@@ -0,0 +1,10 @@
+package tasks.first;
+
+public interface FirstTask {
+
+    String breadthFirst(boolean [][] adjacencyMatrix, int startIndex);
+
+    Boolean validateBrackets(String s);
+
+    Long polishCalculation(String s);
+}
---
+++ src/main/java/tasks/first/FirstTaskSolution.java
@@ -0,0 +1,18 @@
+package tasks.first;
+
+public class FirstTaskSolution implements FirstTask {
+    @Override
+    public String breadthFirst(boolean[][] adjacencyMatrix, int startIndex) {
+        return null;
+    }
+
+    @Override
+    public Boolean validateBrackets(String s) {
+        return null;
+    }
+
+    @Override
+    public Long polishCalculation(String s) {
+        return null;
+    }
+}
